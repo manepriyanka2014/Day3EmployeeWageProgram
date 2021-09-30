1dd17d2051dbf28ddaff92427a5bd88dcb571bd3
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-09-30 22:51:03 +0530

    UC2-Employee Daily Wage

---
+++ EmployeeWageUC2.java
@@ -0,0 +1,19 @@
+public class EmployeeWageUC2 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to EmployeeWage");
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		// Variables
+		int empHrs = 0;
+		int empWage = 0;
+		// Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else
+			empHrs = 5;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage:" + empWage);
+	}
+
+}
